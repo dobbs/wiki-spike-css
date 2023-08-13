7a4dc3ebc733b40b94118955114d6ed4b36b4d9a
Paul Rodwell <paul.rodwell@btinternet.com>
2023-08-13 18:37:07 +0100

    add pagefold

--- pwa/wiki.js
+++ pwa/wiki.js
@@ -148,6 +148,11 @@ window.addEventListener("load", async () => {
           })
           return p
         }
+      },
+      {
+        type: 'pagefold',
+        deps: ['html'],
+        fn: item => html => html`<hr class="pagefold" data-content="${item.text}">`
       }
     ],
     addPanel(panel, replaceId=null) {
